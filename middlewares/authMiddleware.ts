6ed39c28ea32a49c917c048d91485269939e9b9f
muneebsafdar <safdarmuneeb95@gmail.com>
2026-03-25 23:57:11 +0500

    auth middleware added

---
+++ middlewares/authMiddleware.ts
@@ -0,0 +1,16 @@
+
+import {NextFunction, Request, Response} from 'express'
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = async (req: Request, res: Response,next:NextFunction) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1]
+        if(!token){
+            return res.status(401).json({message:"Unauthorized"})
+        }
+        const decodedToken = jwt.verify(token,process.env.JWT_SECRET as string)
+        next()
+    } catch (error) {
+        return res.status(500).json({message:"Internal server error"})
+    }
+}
